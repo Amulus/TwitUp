68826b83b84323d8322bb5038891e25770a84f73
Axel Murat <axel.murat@laposte.net>
2021-03-04 09:21:49 +0100

    fix

--- TwitUp/src/com/iup/tp/twitup/ihm/TwitupTwitView.java
+++ TwitUp/src/com/iup/tp/twitup/ihm/TwitupTwitView.java
@@ -13,7 +13,7 @@ public class TwitupTwitView extends JPanel {
     protected JLabel avatar;
     protected JTextArea textArea;
     protected JLabel tag;
-    protected GridBagConstraints gbc;
+    protected GridBagConstraints gbc = new GridBagConstraints();
 
 
     public TwitupTwitView(String name, String tag, String text, ImageIcon avatar) {
@@ -42,31 +42,29 @@ public class TwitupTwitView extends JPanel {
 
     }
     public void addComposant(){
-        gbc = );
         //avatar
         gbc.gridx = gbc.gridy = 0;
         gbc.gridheight = 2;
         //gbc.anchor = GridBagConstraints.CENTER;
         //gbc.fill = 1;
-        add(avatar,new GridBagConstraints(0, 0, 1, 2, 0, 0, GridBagConstraints.CENTER, GridBagConstraints. , 0, 0, 0);
+        add(avatar,new GridBagConstraints(0, 0, 1, 2, 0, 0, GridBagConstraints.CENTER, 1 , new java.awt.Insets(0,0,0,0), 0, 0));
 
         //name
         gbc.gridx = 1;
         gbc.gridy = 0;
-        add(name,gbc);
+        add(name,new GridBagConstraints(0, 1, 1, 0, 0, 0, GridBagConstraints.CENTER, 1 , new java.awt.Insets(0,0,0,0), 0, 0));
 
         //tag
 
         gbc.gridx = 2;
         gbc.gridy = 1;
-        add(tag,gbc);
+        add(tag,new GridBagConstraints(1, 2, 1, 0, 0, 0, GridBagConstraints.CENTER, 1 , new java.awt.Insets(0,0,0,0), 0, 0));
 
         //text
 
         gbc.gridwidth = 10;
         gbc.gridx = 0;
         gbc.gridy = 5;
-        add(textArea,gbc);
-        
+        add(textArea,new GridBagConstraints(0, 5, 1, 2, 0, 0, GridBagConstraints.CENTER, 1 , new java.awt.Insets(0,0,0,0), 0, 0));
     }
 }
